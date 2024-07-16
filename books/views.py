3af7feea8b5650152aa4bfbaa388d734e34de7b8
Andrii Zubchenko <dissom1987@gmail.com>
2024-07-16 22:14:30 +0300

    Added permissions for diferent endpoints

--- books/views.py
+++ books/views.py
@@ -8,3 +8,17 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            permission_classes = [
+                IsAdminUser,
+            ]
+        elif self.action == "retrieve":
+            permission_classes = [
+                IsAuthenticated,
+            ]
+        else:
+            permission_classes = [
+                AllowAny,
+            ]
+        return [permission() for permission in permission_classes]
