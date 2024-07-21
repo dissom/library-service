bbf47c416616c035748c69860d4a32196a8aa0c1
Andrii Zubchenko <dissom1987@gmail.com>
2024-07-21 17:00:32 +0300

    Added success & cancel endpoints for payment.

--- payment/views.py
+++ payment/views.py
@@ -1,24 +1,27 @@
-from rest_framework import viewsets, mixins
+from rest_framework import (
+    viewsets,
+    mixins,
+)
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from borrowings.helpers.telegram import send_message
 from payment.models import Payment
 from payment.serializers import (
     PaymentSerializer,
-    PaymentDetailSerializer
+    PaymentDetailSerializer,
 )
 
 
 class PaymentViewSet(
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet
+    mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
 ):
     queryset = Payment.objects.all().select_related()
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         queryset = self.queryset
-        queryset = Payment.objects.all().select_related()
         user = self.request.user
         if not user.is_staff:
             return queryset.filter(user=user)
@@ -28,3 +31,34 @@ class PaymentViewSet(
         if self.action == "list":
             return PaymentSerializer
         return PaymentDetailSerializer
+
+
+class PaymentSuccessView(APIView):
+    def get(self, request, *args, **kwargs) -> Response:
+        session_id = request.query_params.get("session_id")
+        payment = Payment.objects.get(session_id=session_id)
+        payment.status = Payment.PaymentStatus.PAID.name
+        payment.save()
+        message = (
+            f"New Borrowing Created:\n"
+            f"Book: {payment.borrowing.book.title} "
+            f"({payment.borrowing.book.author})\n"
+            f"User: {payment.borrowing.user.email}\n"
+            f"Borrow Date: {payment.borrowing.borrow_date}\n"
+            f"Expected Return Date: {payment.borrowing.expected_return_date}"
+        )
+        send_message(message)
+
+        return Response({"detail": "Payment succeeded!"})
+
+
+class PaymentCancelView(APIView):
+
+    def get(self, request, *args, **kwargs) -> Response:
+        return Response(
+            {
+                "detail": "Your payment session is still "
+                "available for 24 hours. Please complete your "
+                "payment within this period."
+            }
+        )
